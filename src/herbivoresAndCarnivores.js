03db6ff6bad67b8061145b45d8bb8843cb4ff9ce
Maxym Modrytskyi <maxmodrr@gmail.com>
2024-10-15 00:40:27 +0300

    results

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -2,14 +2,44 @@
 
 class Animal {
   // write your code here
+  static alive = [];
+
+  constructor(health = 100, name) {
+    this.health = health;
+    this.name = name;
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
-}
+  // write your code here0
+  constructor(name, health = 100, hidden = false) {
+    super(name, health, hidden);
+    this.hidden = hidden;
+    this.health = health;
+    Animal.alive.push(this);
+  }
 
+  hide() {
+    this.hidden = true;
+  }
+}
 class Carnivore extends Animal {
   // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+    this.health = health;
+    Animal.alive.push(this);
+  }
+
+  bite(enemy) {
+    if (enemy instanceof Herbivore && enemy.hidden === false) {
+      enemy.health -= 50;
+
+      if (enemy.health <= 0) {
+        Animal.alive.splice(Animal.alive.indexOf(enemy), 1);
+      }
+    }
+  }
 }
 
 module.exports = {
